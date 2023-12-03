5ba6caf47e9c614d4fcfa2ee204fd760a424c654
Sailmaster91 <112721254+Sailmaster91@users.noreply.github.com>
2023-12-02 21:56:26 -0400

    aside remplace tag sidebar in html

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,29 +1,27 @@
 'use strict';
 
-const navMovilBoton = document.querySelector('#nav-mobile__button');
-const navMobile = document.querySelector('.nav-mobile');
-const closeButton = document.querySelector('.nav-mobile button');
+const navMovilBoton = document.getElementById('nav-mobile__button');
 
 navMovilBoton.addEventListener('click', (e) => {
 	e.preventDefault();
 
 	if (e.target.closest('button')) {
 		document.querySelector('.overlay').classList.add('overlay--active');
-
+		const overlay = document.querySelector('.nav-mobile').parentElement;
 		setTimeout(
 			() => (document.querySelector('.overlay--active').style.transform = 'translateX(0%)'),
 			10
 		);
-	}
-});
-const overlay = document.querySelector('.nav-mobile').parentElement;
-overlay.addEventListener('click', (e) => {
-	if (!navMobile.contains(e.target) || closeButton.contains(e.target)) {
-		document.querySelector('.overlay--active').style.transform = 'translateX(100%)';
-		setTimeout(
-			() => document.querySelector('.overlay').classList.remove('overlay--active'),
+		overlay.addEventListener('click', (e) => {
+			setTimeout(
+				() => (document.querySelector('.overlay--active').style.transform = 'translateX(100%)'),
+				10
+			);
+			setTimeout(
+				() => document.querySelector('.overlay').classList.remove('overlay--active'),
 
-			300
-		);
+				20
+			);
+		});
 	}
 });
